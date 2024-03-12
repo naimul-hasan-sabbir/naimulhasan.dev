4a95570291d13c73a94aca4a7bb56f978333e9c9
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-03-12 11:46:10 +0600

    js file initialized and icon styling

---
+++ app.js
@@ -0,0 +1,4 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
