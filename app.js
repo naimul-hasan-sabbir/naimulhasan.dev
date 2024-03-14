0e0f6dd64f30684ae16f5948c1843e44b2d62e2d
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-03-14 12:51:15 +0600

    active class working on each section buttons

--- app.js
+++ app.js
@@ -12,6 +12,22 @@ function pageTransitions(){
             this.className += ' active-btn';
         })
     }
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+
+        if(id){
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 pageTransitions();
